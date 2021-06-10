0adf57e6ca07db30234cbc0e7687dbadca39102e
Maika Van Der Sluis <maikav01@nssrstudio02.csa.scot.nhs.uk>
2021-06-10 15:02:48 +0100

    Added a step

---
+++ Step_AgeCalc.R
@@ -0,0 +1,11 @@
+#Calculating age based on the date of birth (DOB)
+#DOB = date of birth
+#enddate = defaults to current date
+#units = in this case years
+#precise = whether or not to calculate with leap year 
+TRUE_SAMPLE <- data_normal %>%
+  age_calc(dob, enddate = Sys.Date(), units = "months", precise = TRUE) %>%
+  select(-dob)
+
+
+
