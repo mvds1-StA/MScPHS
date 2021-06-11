ee43c368f1e21642fb5e09342603a3ddb2c071e7
Maika Van Der Sluis <maikav01@nssrstudio05.csa.scot.nhs.uk>
2021-06-11 09:50:45 +0100

    Added a few lines

--- Step_AgeCalc.R
+++ Step_AgeCalc.R
@@ -3,9 +3,11 @@
 #enddate = defaults to current date
 #units = in this case years
 #precise = whether or not to calculate with leap year 
-TRUE_SAMPLE <- data_normal %>%
+data_normal %>%
   age_calc(dob, enddate = Sys.Date(), units = "months", precise = TRUE) %>%
   select(-dob)
+
+save(file=sprintf("dat/01b_TRUESAMPLE.Rdat"))
 
 
 
---
+++ Step_SimData.R
@@ -0,0 +1,4 @@
+
+mutate(SIM_SAMPLE, rceruited = FALSE , TRUE)
+for (i in SIM_SAMPLE): 
+  if x 
